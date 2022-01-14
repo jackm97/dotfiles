0af2a4d0864cce1dac258d4b94cc38fad047c362
Jack Myers <jackhmyers97@gmail.com>
2022-01-13 22:36:22 -0800

    init config files

---
+++ .config/i3/lock.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+BLANK='#00000000'
+CLEAR='#ffffff22'
+DEFAULT='#d9a0facc'
+TEXT='#d9a0faee'
+WRONG='#d76e78bb'
+VERIFYING='#EBBE6B'
+
+i3lock \
+--insidever-color=$CLEAR     \
+--ringver-color=$VERIFYING   \
+\
+--insidewrong-color=$CLEAR   \
+--ringwrong-color=$WRONG     \
+\
+--inside-color=$BLANK        \
+--ring-color=$DEFAULT        \
+--line-color=$BLANK          \
+--separator-color=$DEFAULT   \
+\
+--verif-color=$VERIFYING     \
+--wrong-color=$WRONG         \
+--time-color=$TEXT           \
+--date-color=$TEXT           \
+--layout-color=$TEXT         \
+--keyhl-color=$WRONG         \
+--bshl-color=$WRONG          \
+\
+--screen 1                   \
+--blur 5                     \
+--clock                      \
+--indicator                  \
+--time-str="%H:%M:%S"        \
+--date-str="%A, %m %Y"       \
