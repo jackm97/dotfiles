e18b11e5bd942e5b04adf7ec9a4fed7613713ad1
Jack Myers <jackhmyers97@gmail.com>
2022-01-15 11:39:56 -0800

    added oh my zsh to install script

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
+#i3 related stuff + alacritty
 rm -rf ~/.config/rofi ~/.config/i3 ~/.config/i3blocks ~/.config/picom ~/.config/dunst ~/.config/alacritty
 ln -s $PWD/.config/rofi ~/.config/rofi
 ln -s $PWD/.config/i3 ~/.config/i3
@@ -7,3 +8,9 @@ ln -s $PWD/.config/i3blocks ~/.config/i3blocks
 ln -s $PWD/.config/picom ~/.config/picom
 ln -s $PWD/.config/dunst ~/.config/dunst
 ln -s $PWD/.config/alacritty ~/.config/alacritty
+
+#oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+cp .zshrc ~/.zshrc
