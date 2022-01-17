56c905d215e3fe5352f9dc834e8a501cb965cf77
Jack Myers <jackhmyers97@gmail.com>
2022-01-17 11:50:33 -0800

    changed system font, changed a color in alacritty, removed blur and cleaned up

--- install.sh
+++ install.sh
@@ -10,10 +10,13 @@ ln -s $PWD/.config/dunst ~/.config/dunst
 ln -s $PWD/.config/alacritty ~/.config/alacritty
 
 #oh my zsh
+rm -rf ~/.oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-cp .zshrc ~/.zshrc
+rm ~/.zshrc
+ln -s $PWD/.zshrc ~/.zshrc
 
 #.zshenv
-cp .zshenv ~/zshenv
+rm ~/.zshenv
+ln -s $PWD/.zshenv ~/.zshenv
